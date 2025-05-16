47ef0b981122e791de228490cde06e61239189cc
ariss99nn <rodrigueznilo99@gmail.com>
2025-05-16 18:37:29 -0500

    Harvest Code

--- Cosecha/serializers.py
+++ Cosecha/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
-from .models import Harvest, Harvest_type, Report_Harvest
+from .models import Harvest, Classification, HarvestType, HarvestDetails
+
+class Classification_Serializer(serializers.Serializer):
+    class Meta:
+        model = Classification
+        fields = '__all__'
+
 
 class Harvest_Serializer(serializers.Serializer):#creamos el serilizador de cosecha
     class Meta:#clase para mostrar los datos
@@ -8,10 +14,10 @@ class Harvest_Serializer(serializers.Serializer):#creamos el serilizador de cose
         
 class Harvest_type_Serializer(serializers.Serializer):
     class Meta:#clase para mostrar los datos
-        model = Harvest_type#seleccionamos el modelo de tipo cosecha
+        model = HarvestType#seleccionamos el modelo de tipo cosecha
         fields = 'all'#los campos que se van a mostrar(ALL SE REIERE A TODOS LOS CAMPOS)
         
-class Report_Harvest_Serializer(serializers.Serializer):
-    class Meta:#clase para mostrar los datos
-        model = Report_Harvest#seleccionamos el modelo de reporte de cosecha
-        fields = '__all__'#los campos que se van a mostrar(ALL SE REIERE A TODOS LOS CAMPOS)
+class HarvestDeatils_Serializers(serializers.Serializer):
+    class Meta:
+        model=HarvestDetails
+        fields = '__all__'
