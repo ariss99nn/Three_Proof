65897b817c550bfdb6fd6a9c145d24b225fc20f6
ariss99nn <rodrigueznilo99@gmail.com.com>
2025-05-21 18:19:16 -0500

    Views Prediccion

--- Prediccion/views.py
+++ Prediccion/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Prediction
+from .serializers import PredictionSerializer
 # Create your views here.
+
+class predictionView(APIView):
+    def get(self, request):
+        prediccion = Prediction.objects.all()
+        serializer = PredictionSerializer(prediccion, many=True)
+        return Response(serializer.data)
